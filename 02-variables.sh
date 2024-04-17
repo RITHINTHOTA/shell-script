41113e4e70b85db654266e94ea460e5f7da870c1
RITHINTHOTA <thotasrisairithin@gmail.com>
2024-04-17 10:41:56 +0530

    commit

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh" :: Hello Suresh, How are you?
+echo "Suresh" :: Hi Ramesh, Iam fine
+echo  "Ramesh" :: what about your job Suresh?
+echo  "Suresh" :: Still, Searching Ramesh
