6689d60f2b5c2e7a667a79248d0e0e676b16b170
RITHINTHOTA <thotasrisairithin@gmail.com>
2024-04-18 19:08:27 +0530

    special

--- 12-conditions.sh
+++ 12-conditions.sh
@@ -3,7 +3,7 @@
 NUMBER=$1
 if [ $NUMBER -le 10 ]
 then 
-echo "Given Number $NUMBER is lessthanorequal 10"
+echo "Given Number $NUMBER is lessthan or equal 10"
 else
-echo "Given Number $NUMBER is not lessthanorequal 10"
+echo "Given Number $NUMBER is not lessthan or equal 10"
 fi
