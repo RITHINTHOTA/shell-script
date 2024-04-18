2f27d040f1252146820a3212bf4ba68ee13801fc
RITHINTHOTA <thotasrisairithin@gmail.com>
2024-04-18 19:04:11 +0530

    special

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -2,9 +2,9 @@
 #!/bin/bash
 
 NUMBER=$1
-if [$NUMBER -lt 20]
+if [ $NUMBER -lt 10 ]
 then 
-echo "Given Number $NUMBER is < 20"
+echo "Given Number $NUMBER is lessthan 10"
 else
-echo "Given Number $NUMBER is > 20"
+echo "Given Number $NUMBER is greaterthan 10"
 fi
