eab6342fb1401249d6bc6d96b7c9c0cae25108f3
RITHINTHOTA <thotasrisairithin@gmail.com>
2024-04-18 18:43:34 +0530

    special

--- 09-specialvariables.sh
+++ 09-specialvariables.sh
@@ -2,3 +2,8 @@
 
 echo "All variables: $@"
 echo "Tota number of variables $#"
+echo "Script Name: $0"
+echo "current working directory $PWD"
+echo "Home directory $HOME"
+echo "which user is running this script $USER"
+echo "Hostname $HOSTNAME"
