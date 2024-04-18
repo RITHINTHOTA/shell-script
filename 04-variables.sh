dec9db5d21820bdd69112b4607798f9c4e46fd3d
RITHINTHOTA <thotasrisairithin@gmail.com>
2024-04-18 11:01:34 +0530

    variable

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1" :: Hello $PERSON2 , How are you?
+echo "$PERSON2" :: Hi $PERSON1, Iam fine
+echo  "$PERSON1" :: what about your job $PERSON2?
+echo  "$PERSON2" :: Still, Searching $PERSON1
