21cef4f8cfd8fb7869d22556ad7506d600a47cce
RITHINTHOTA <thotasrisairithin@gmail.com>
2024-04-19 15:18:00 +0530

    Installation

---
+++ 14-functions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2....FAILURE"
+        exit 1
+        echo "$2....SUCESS"
+    fi
+}
+
+USERID=$( id -u )
+   if [ $USERID -ne 0 ]
+   then
+       echo "please run this with root access"
+       exit 1
+    else
+      echo "you are a root user"
+    fi
+    
+    dnf install mysql -y
+    VALIDATE "$?" "Installing sucess"
+
+    dnf install git -y
+    VALIDATE "$?" "Installing sucess"
+ 
