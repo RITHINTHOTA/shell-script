ece2dbaa33f67bff4d5a3d026a26e1cbe21e9adc
RITHINTHOTA <thotasrisairithin@gmail.com>
2024-04-20 02:08:40 +0530

    commit

--- 15-logs.sh
+++ 15-logs.sh
@@ -14,9 +14,10 @@ VALIDATE(){
 }
 
 USERID=$( id -u )
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
     echo "please run this with root access"
+    exit 1
 else
     echo "you are a root user"
 fi
