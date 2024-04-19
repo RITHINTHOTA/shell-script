ca21be1427160ec08207751a0b42179fc73adde9
RITHINTHOTA <thotasrisairithin@gmail.com>
2024-04-20 02:23:54 +0530

    commit

--- 16-colors.sh
+++ 16-colors.sh
@@ -10,9 +10,9 @@ N="\e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2...  $R FAILURE $N "
+        echo -e "$2...$R FAILURE $N "
     else
-        echo "$2.... $G SUCCESS $N "
+        echo -e "$2.... $G SUCCESS $N "
     fi
 }
 
