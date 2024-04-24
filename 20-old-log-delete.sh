76ee84cd0f3560132362d49788e7cbb15ab1182b
RITHINTHOTA <thotasrisairithin@gmail.com>
2024-04-24 15:24:01 +0530

    commit

--- 20-old-log-delete.sh
+++ 20-old-log-delete.sh
@@ -17,10 +17,8 @@ fi
 
 FILES=$(find $SOURCE_DIRECTORY -name "* .log" -mtime +14)
 
-echo "Files to delete :$FILES"
-
 while IFS= read -r line
 do
   echo "Deleting file :$line"
- rm -rf $line
+   rm -rf $line
  done <<< $FILES
